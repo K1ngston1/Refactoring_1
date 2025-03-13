833c58b649ce408ea16db99a058765edbde7332d
K1ngst0n1 <kingstn29@gmail.com>
2025-03-13 14:56:48 +0200

    "Add README.md "

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+        HumanIMB human = new HumanIMB(80, 1.52);
+        System.out.println("ІМТ: " + human.getIMB());
+        System.out.println("Результат: " + human.getResult());
+    }
+}
+
+class HumanIMB {
+    private double weight;
+    private double height;
+
+    public HumanIMB(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getIMB() {
+        return weight / (height * height);
+    }
+
+    public String getResult() {
+        double imb = getIMB();
+        if (imb < 18.5) {
+            return "Deficit";
+        } else if (imb < 25) {
+            return "Norm";
+        } else if (imb < 30) {
+            return "Warning!";
+        } else {
+            return "Fat";
+        }
+    }
+}
